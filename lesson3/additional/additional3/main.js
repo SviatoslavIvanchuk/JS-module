6c0693e7e89653b4045922646444384ad661a008
SviatoslavIvanchuk <isv1796@gmail.com>
2021-11-17 11:17:06 +0200

    lesson3--additional3add

--- lesson3/additional/additional3/main.js
+++ lesson3/additional/additional3/main.js
@@ -3,7 +3,7 @@
 //     b. заповнити його 50 непарними числами за допомоги циклу.
 //     c. Заповнити масив 20ма рандомними числами. (Google: Generate random number JS)
 //     d. Заповнити масив 20ма рандомними чисалами в діапазоні від 8 до 732 (Google: Generate random number JS)
-
+//
 //     let firstArray = [];
 //     for (let i = 0; i < 50; i++) {
 //        firstArray.push(i*2+2);
@@ -35,16 +35,18 @@
 //             console.log(firstArray[i])
 //         }
 //     }
-//
+// //
 //     console.log('4. Вивести за допомогою console.log кожен третій елемен тільки якщо цей елемент є парним та записати їх в новий масив')
-// // 4. Вивести за допомогою console.log кожен третій елемен тільки якщо цей елемент є парним та записати їх в новий масив
-//
+// // // 4. Вивести за допомогою console.log кожен третій елемен тільки якщо цей елемент є парним та записати їх в новий масив
+// //
 //     let secondArray = [];
 //     for (let i = 0; i < firstArray.length; i+= 3) {
-//          if (firstArray[i] % 2 === 0) {
-//             secondArray.push(firstArray[i])
-//          }
-//     }
+//         if (firstArray[i] % 2 === 0) {
+//             // console.log(firstArray[i])
+//             secondArray[i] = firstArray[i];
+//         console.log(secondArray[i])
+//         }
+//     }   console.log(secondArray)
 
 
 
@@ -64,18 +66,82 @@
 
 // 6. Є масив з числами [100,250,50,168,120,345,188], Які характеризують вартість окремої покупки. Обрахувати середній чек.
 
-    let price = [100,250,50,168,120,345,188];
-    price.sort(function(a, b) {
-        return a - b;
-    }); console.log(price);
-    for (let i = 0; i < price.length; i++) {
-
-    }
+    // let price = [100,250,50,168,120,345,188];
+    // price.sort(function(a, b) {
+    //     return a - b;
+    // }); console.log(price);
 
 
 
 // 7. Створити масив з рандомними значеннями, помножити всі його елементи на 5 та перемістити їх в інший масив.
+
+    // let thirdArray = [];
+    // let thirdArrayX5 = [];
+    // for (let i = 0; i < 10; i++) {
+    //     thirdArray.push(Math.floor(Math.random() * 30))
+    //     thirdArrayX5[i] = thirdArray[i]*5;
+    // }   console.log(thirdArray)
+    //    console.log(thirdArrayX5)
+
 // 8. Створити масив з будь якими значеннями (стрінги, числа, і тд...). пройтись по ньому, і якщо елемент є числом - додати його в інший масив.
+
+    // let fourthArray = ['one', 'three', 1, false, 2, 'five', true, 10, 15, 'burger', 'air'];
+    // let fourthArrayNumbers = [];
+    // for (let i = 0; i < fourthArray.length; i++) {
+    //     if (typeof fourthArray[i] === 'number') {
+    //         // console.log(fourthArray[i])
+    //         fourthArrayNumbers[i] = fourthArray[i];
+    //         console.log(fourthArrayNumbers[i]);
+    //     }
+    // }
+
+
+
+// - Дано 2 масиви з рівною кількістю об'єктів.
+// Масиви:
+//     З'єднати в один об'єкт користувача та місто з відповідними "id" та "user_id" .
+//     Записати цей об'єкт в новий масив
+
+// let usersWithId = [
+//     {id: 1, name: 'vasya', age: 31, status: false},
+//     {id: 2, name: 'petya', age: 30, status: true},
+//     {id: 3, name: 'kolya', age: 29, status: true},
+//     {id: 4, name: 'olya', age: 28, status: false}
+// ];
+//
+// let citiesWithId = [
+//     {user_id: 3, country: 'USA', city: 'Portland'},
+//     {user_id: 1, country: 'Ukraine', city: 'Ternopil'},
+//     {user_id: 2, country: 'Poland', city: 'Krakow'},
+//     {user_id: 4, country: 'USA', city: 'Miami'}
+// ];
+
+
+
+
+
+
+
+// - Взяти масив з 10 чисел або створити його. Вивести в консоль тільки ті елементи, значення яких є парними.
+
+    // let arrNumber = [];
+    // for (let i = 0; i < 10; i++) {
+    //     arrNumber.push(Math.floor(Math.random()*50) + 5)
+    //     if (arrNumber[i] % 2 === 0) {
+    //         console.log(arrNumber[i])
+    //     }
+    // }   console.log(arrNumber)
+
+
+
+// - Взяти масив з 10 чисел або створити його. Створити 2й порожній масив. За допомогою будь-якого циклу скопіювати значення одного масиву в інший.
+
+    // let arrNumberTwo = [];
+    // let arrNumberTwoEmpty = [];
+    // for (let i = 0; i < 10; i++) {
+    //     arrNumberTwo.push(Math.floor(Math.random()*50)+50)
+    //     arrNumberTwoEmpty[i] = arrNumberTwo[i]
+    // }   console.log(arrNumberTwoEmpty)
 
 
 
