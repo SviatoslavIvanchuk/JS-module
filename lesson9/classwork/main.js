9c00bae65f4b295e9b48beaf3490f1a15e47bdf6
SviatoslavIvanchuk <isv1796@gmail.com>
2021-12-02 11:24:42 +0200

    lesson9-classwork

---
+++ lesson9/classwork/main.js
@@ -0,0 +1,182 @@
+// - є масив
+let simpsons = [
+    {
+        name: 'Bart',
+        surname: 'Simpson',
+        age: 10,
+        info: 'Бартолом\'ю ДжоДжо «Барт» Сімпсон (англ. Bartholomew JoJo «Bart» Simpson) — один із головних героїв мультиплікаційного серіалу Сімпсони. Барт — найстарша дитина Гомера і Мардж Сімпсон. У нього також є дві молодші сестри — Ліса і Меґґі. Барт є втіленням образу бешкетника та посереднього учня у школі. Разом зі своїм батьком Барт є одним із найвідоміших персонажів у цьому серіалі.',
+        photo: 'https://upload.wikimedia.org/wikipedia/uk/a/aa/Bart_simpson.png'
+    },
+    {
+        name: 'Homer',
+        surname: 'Simpson',
+        age: 40,
+        info: 'Гомер Джей Сімпсон (англ. Homer Jay Simpson) — один із головних героїв мультсеріалу «Сімпсони». Гомер — грубий і неввічливий батько родини, він має очевидні вади: товстий, лисий і не дуже розумний. Нерідко він поводиться як блазень, абсурдно, егоїстично і нетактовно, але все ж лишається симпатичним.',
+        photo: 'https://upload.wikimedia.org/wikipedia/en/0/02/Homer_Simpson_2006.png'
+    },
+    {
+        name: 'Marge',
+        surname: 'Simpson',
+        age: 38,
+        info: 'Ма́рджори Жакли́н «Мардж» Си́мпсон (в девичестве Бувье́) (англ. Marjorie Jacqueline «Marge» Simpson) — постоянный персонаж мультипликационного сериала «Симпсоны», её озвучивает Джулия Кавнер. Обычно носит зелёное платье, красные балетки, на шее — ожерелье из искусственного жемчуга и ездит на оранжевом универсале. У неё шикарные синие волосы, которые она обычно собирает в очень высокую причёску. Глаза цвета ореха (19s6e). Основное занятие — домохозяйка, большую часть времени проводит в заботе о доме, детях и Гомере. Образ Мардж копирует стереотип провинциальной американской домохозяйки 50-х годов. Мардж — единственный член семьи, посещающий церковь добровольно. Старается поддерживать нравственность не только своей семьи, но и всего города. Отлично готовит, особенно славятся её свиные отбивные и зефир. Любимое блюдо — лапша с маслом.',
+        photo: 'https://upload.wikimedia.org/wikipedia/ru/0/0b/Marge_Simpson.png'
+    },
+    {
+        name: 'Lisa',
+        surname: 'Simpson',
+        age: 9,
+        info: 'Ли́за Мари́ Си́мпсон (англ. Lisa Marie Simpson) — героиня мультипликационного сериала «Симпсоны». Средний ребёнок в семье, восьмилетняя девочка, выделяющаяся среди остальных Симпсонов прежде всего своим умом и рассудительностью.',
+        photo: 'https://upload.wikimedia.org/wikipedia/ru/e/ec/Lisa_Simpson.png'
+    },
+    {
+        name: 'Maggie',
+        surname: 'Simpson',
+        age: 1,
+        info: 'Ма́ргарет Эвелин «Мэ́гги» Си́мпсон (англ. Margaret Evelyn “Maggie” Simpson) — персонаж мультсериала «Симпсоны». Впервые появилась на телевидении в шоу Трейси Ульман, в короткометражке Good Night (англ.)русск. 19 апреля 1987 года. Мэгги была придумана и разработана карикатуристом Мэттом Грейнингом, пока он ждал встречи с Джеймсом Л. Бруксом. Названа в честь младшей сестры Грейнинга. После появления в шоу Трейси Ульман, через три года семья Симпсонов получила собственный сериал на телеканале Fox, дебют произошёл 17 декабря 1989 года.',
+        photo: 'https://upload.wikimedia.org/wikipedia/ru/9/9d/Maggie_Simpson.png'
+    },
+];
+//
+// - взяти попередній масив з сімпсонами.
+//     Проітерувати його, створиши для кожного елементу масиву <div class='member'>. Для кожної властивості елементу створити окремий блок, та помістити його у div.member
+
+let simpsonsBloc = document.createElement('div')
+document.body.appendChild(simpsonsBloc)
+document.write('<hr/>')
+document.write('<hr/>')
+
+for (const simpsonPersonage of simpsons) {
+    let personage = document.createElement('div');
+    personage.classList.add('member')
+
+    let hrLine = document.createElement('hr')
+
+    let h3 = document.createElement('h3');
+    h3.innerText = simpsonPersonage.name + simpsonPersonage.surname + ' ages: ' + simpsonPersonage.age
+    let p = document.createElement('p');
+    p.innerText = simpsonPersonage.info
+    let img = document.createElement('img')
+    img.src = simpsonPersonage.photo
+
+    personage.appendChild(img)
+    personage.appendChild(h3)
+    personage.appendChild(p)
+    personage.appendChild(hrLine)
+
+    simpsonsBloc.appendChild(personage)
+
+}
+
+
+
+// - Є масив
+let coursesArray = [
+    {
+        title: 'JavaScript Complex',
+        monthDuration: 5,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'node.js']
+    },
+    {
+        title: 'Java Complex',
+        monthDuration: 6,
+        hourDuration: 909,
+        modules: ['html',
+            'css',
+            'js',
+            'mysql',
+            'mongodb',
+            'angular',
+            'aws',
+            'docker',
+            'git',
+            'java core',
+            'java advanced']
+    },
+    {
+        title: 'Python Complex',
+        monthDuration: 6,
+        hourDuration: 909,
+        modules: ['html',
+            'css',
+            'js',
+            'mysql',
+            'mongodb',
+            'angular',
+            'aws',
+            'docker',
+            'python core',
+            'python advanced']
+    },
+    {
+        title: 'QA Complex',
+        monthDuration: 4,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'git', 'QA/QC']
+    },
+    {
+        title: 'FullStack',
+        monthDuration: 7,
+        hourDuration: 909,
+        modules: ['html',
+            'css',
+            'js',
+            'mysql',
+            'mongodb',
+            'react',
+            'angular',
+            'aws',
+            'docker',
+            'git',
+            'node.js',
+            'python',
+            'java']
+    },
+    {
+        title: 'Frontend',
+        monthDuration: 4,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'sass']
+    }
+];
+// Створити для кожного елементу масиву свій блок, блок розділити блоками, в яких будуть зберігатись значення окремих властивостей, для властивості modules зробити список з елементами
+// Приклад структири знаходиться у файлі example.png
+
+
+let coursesBloc = document.createElement('div')
+document.body.appendChild(coursesBloc)
+
+for (const element of coursesArray) {
+    let blocWithCourse = document.createElement('div')
+    blocWithCourse.style.border = '2px solid aqua'
+    blocWithCourse.style.margin = '5px'
+
+    let blocTitle = document.createElement('h4');
+        blocTitle.style.margin = '2px'
+        blocTitle.innerText = element.title;
+    let blockMonthAndHour = document.createElement('div')
+    blockMonthAndHour.style.display = 'flex';
+    blockMonthAndHour.style.columnGap = '10px';
+        let blocMonth = document.createElement('h5');
+        blocMonth.style.margin = '2px'
+        blocMonth.innerText = 'Курс триває ' + element.monthDuration + ' місяців.';
+        let blocHour = document.createElement('h5');
+        blocHour.style.margin = '2px'
+        blocHour.innerText = 'Всього ' + element.hourDuration + ' годин'
+
+    let blocModules = document.createElement('div')
+        for (const moduleItem of element.modules) {
+        let blocModulesLi = document.createElement('li');
+        blocModulesLi.innerText = moduleItem;
+
+        blocModules.appendChild(blocModulesLi)
+        }
+
+    blocWithCourse.appendChild(blocTitle)
+    blocWithCourse.appendChild(blockMonthAndHour)
+        blockMonthAndHour.appendChild(blocMonth)
+        blockMonthAndHour.appendChild(blocHour)
+    blocWithCourse.appendChild(blocModules)
+
+    coursesBloc.appendChild(blocWithCourse)
+}
